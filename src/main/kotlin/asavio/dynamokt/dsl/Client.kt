f42d556822754c9b5601f086e5891b86c24ceb73
aseemsavio <aseemsavio3@gmail.com>
2022-12-28 20:18:42 +0530

    added client comment

--- src/main/kotlin/asavio/dynamokt/dsl/Client.kt
+++ src/main/kotlin/asavio/dynamokt/dsl/Client.kt
@@ -5,6 +5,8 @@ import asavio.dynamokt.services.DynamoDbClientBuilder
 
 /**
  * This DSL creates a DynamoDB [Client] object. Accepts accessKey, secretKey and region.
+ * The returned client object can be used to make further operations on.
  *
  */
 fun dynamoClient(fn: DynamoDbClientBuilder.() -> Unit): Client = DynamoDbClientBuilder().apply(fn).build()
+
