5da39d0a2fe9b4e6cf30094d9d93c87b35ce39c5
aseemsavio <aseemsavio3@gmail.com>
2023-08-25 17:36:04 +0530

    variables

--- src/main/kotlin/com/aseemsavio/dynamokt/utils/elements.kt
+++ src/main/kotlin/com/aseemsavio/dynamokt/utils/elements.kt
@@ -1,32 +1,51 @@
 package com.aseemsavio.dynamokt.utils
 
 import javax.annotation.processing.ProcessingEnvironment
-import javax.lang.model.element.Element
-import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
 import javax.lang.model.element.VariableElement
+import javax.lang.model.type.DeclaredType
+import javax.lang.model.type.TypeMirror
 
-/**
- * Returns an element as a [TypeElement] if the element is of kind [ElementKind.CLASS].
- */
-internal val Element.asTypeElementIfClass: TypeElement?
-    get() = if (kind != ElementKind.CLASS) {
-        null
-    } else {
-        this as TypeElement
-    }
 
 /**
  * Gets all the variables in the elements
  */
-internal fun <T : Annotation> variables(
+internal fun variables(
     element: TypeElement,
     processingEnv: ProcessingEnvironment,
-    annotation: Class<T>
 ) = element.enclosedElements
     .filterIsInstance<VariableElement>()
-    .associate { variable ->
-        val type = processingEnv.typeUtils.asElement(variable.asType()) as? TypeElement
-        val hasAnnotation = type?.getAnnotation(annotation) != null
-        variable.simpleName.toString() to type.takeIf { hasAnnotation }
+    .map {
+        javaType(
+            variable = it,
+            processingEnv = processingEnv
+        )
     }
+
+internal fun javaType(variable: VariableElement, processingEnv: ProcessingEnvironment): JvmTypeElement {
+    val simpleName = variable.simpleName.toString()
+    val type = typeElement(variable, processingEnv)
+    return JvmTypeElement(
+        name = simpleName,
+        fullyQualifiedType = type.first,
+        typeArgument = type.second?.firstOrNull()
+    )
+}
+
+internal fun typeElement(
+    variable: VariableElement,
+    processingEnv: ProcessingEnvironment
+): Pair<TypeElement, List<TypeMirror>?> {
+    val typeMirror = variable.asType()
+    return if (typeMirror is DeclaredType) {
+        Pair(typeMirror.asElement() as TypeElement, typeMirror.typeArguments)
+    } else {
+        Pair((processingEnv.typeUtils.asElement(typeMirror) as? TypeElement)!!, null)
+    }
+}
+
+data class JvmTypeElement(
+    val name: String,
+    val fullyQualifiedType: TypeElement,
+    val typeArgument: TypeMirror?
+)
