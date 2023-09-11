5f89db61b7bc35d5ffac6ecf2af6b696dcd24639
aseemsavio <aseemsavio3@gmail.com>
2023-09-11 08:05:22 +0530

    gsi, lsi, provisioned throughput, sse DSLs added

--- src/main/kotlin/com/aseemsavio/dynamokt/extensions/crud/sync/tables/Create.kt
+++ src/main/kotlin/com/aseemsavio/dynamokt/extensions/crud/sync/tables/Create.kt
@@ -1,20 +1,23 @@
 package com.aseemsavio.dynamokt.extensions.crud.sync.tables
 
+import software.amazon.awssdk.core.waiters.WaiterResponse
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition
-import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest
-import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement
+import software.amazon.awssdk.services.dynamodb.model.*
+
 
 // https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-dynamodb.html
 
-fun createTable() {
-    val a = CreateTableRequest.builder()
-
-}
-
-fun DynamoDbClient.newTable(block: CreateTableRequest.Builder.() -> Unit) {
-    val request = CreateTableRequest.builder().apply(block).build()
-    createTable(request)
+fun DynamoDbClient.newTable(block: CreateTableRequest.Builder.() -> Unit): TableName {
+    val waiter = waiter()
+    val createTableRequest = CreateTableRequest.builder().apply(block).build()
+    val tableName = createTableRequest.tableName()
+    val createTableResponse = createTable(createTableRequest)
+    val describeTableRequest = DescribeTableRequest.builder()
+            .tableName(tableName)
+            .build()
+    val waiterResponse: WaiterResponse<DescribeTableResponse> = waiter.waitUntilTableExists(describeTableRequest)
+    waiterResponse.matched().response().ifPresent { println(it) }
+    return TableName(createTableResponse.tableDescription().tableName())
 }
 
 /**
@@ -28,3 +31,35 @@ fun attributeDefinition(block: AttributeDefinition.Builder.() -> Unit): Attribut
  */
 fun keySchemaElement(block: KeySchemaElement.Builder.() -> Unit): KeySchemaElement =
         KeySchemaElement.builder().apply(block).build()
+
+/**
+ * DSL for creating a [GlobalSecondaryIndex].
+ */
+fun globalSecondaryIndex(block: GlobalSecondaryIndex.Builder.() -> Unit): GlobalSecondaryIndex =
+        GlobalSecondaryIndex.builder().apply(block).build()
+
+/**
+ * DSL for creating a [LocalSecondaryIndex].
+ */
+fun localSecondaryIndex(block: LocalSecondaryIndex.Builder.() -> Unit): LocalSecondaryIndex =
+        LocalSecondaryIndex.builder().apply(block).build()
+
+/**
+ * DSL for creating [SSESpecification].
+ */
+fun CreateTableRequest.Builder.sseSpecificationDynamoKt(block: SSESpecification.Builder.() -> Unit) = apply {
+    sseSpecification(SSESpecification.builder().apply(block).build())
+}
+
+/**
+ * DSL for creating [ProvisionedThroughput].
+ */
+fun CreateTableRequest.Builder.provisionedThroughputDynamoKt(block: ProvisionedThroughput.Builder.() -> Unit) = apply {
+    provisionedThroughput(ProvisionedThroughput.builder().apply(block).build())
+}
+
+/**
+ * [TableName] is a [String].
+ */
+@JvmInline
+value class TableName(val value: String)
